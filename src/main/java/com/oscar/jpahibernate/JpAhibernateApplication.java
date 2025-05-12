20cdab6806f9150e41c56a552a85f26ab18f4cec
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-12 09:48:22 +0200

    First commit

---
+++ src/main/java/com/oscar/jpahibernate/JpAhibernateApplication.java
@@ -0,0 +1,13 @@
+package com.oscar.jpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAhibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpAhibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/oscar/jpahibernate/JpAhibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oscar.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAhibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
