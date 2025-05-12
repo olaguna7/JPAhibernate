65ad24cd911a41d32de4a5bbf2812f2c77f31f84
olaguna7 <oscarlagunacaminero@gmail.com>
2025-05-12 09:54:33 +0200

    Add class of package entities

---
+++ src/main/java/com/oscar/jpahibernate/entities/Cliente.java
@@ -0,0 +1,46 @@
+package com.oscar.jpahibernate.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table("cliente")
+public class Cliente {
+    @Id
+    @Column("codigo_cliente")
+    private int codigoCliente;
+    @Column("nombre_cliente")
+    private String nombreCliente;
+    @Column("nombre_contacto")
+    private String nombreContacto;
+    @Column("apellido_contacto")
+    private String apellidoContacto;
+    private String telefono;
+    private String fax;
+    @Column("linea_direccion1")
+    private String lineaDireccion1;
+    @Column("linea_direccion2")
+    private String lineaDireccion2;
+    private String ciudad;
+    private String region;
+    private String pais;
+    @Column("codigo_postal")
+    private String codigoPostal;
+    @Column("codigo_empleado_rep_ventas")
+    private Integer codigoRepresentanteVentas;
+    @Column("limite_credito")
+    private Double limiteCredito;
+
+    @Override
+    public String toString() {
+        return "Cliente: " + nombreCliente + " telefono: " + telefono;
+    }
+}
